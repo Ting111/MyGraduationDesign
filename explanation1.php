9ef2a81f3f478813ae05c7bcc86d6f2ac65d21dc
Ting <wlovechu@qq.com>
2018-01-25 13:16:39 +0800

    preparatory work

---
+++ explanation1.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>短信注册</title>
+  <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.css">
+</head>
+<body>
+<?php
+include 'nav.html';
+?>
+<div class="container">
+  <div class="jumbotron">
+    <p>text content</p>
+  </div>
+</div>
+</body>
+</html>
---
+++ explanation3.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>登录异常反馈</title>
+  <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.css"></head>
+<body>
+<?php
+include 'nav.html';
+?>
+<div class="container">
+  <div class="jumbotron">
+    <p>text content</p>
+  </div>
+</div>
+</body>
+</html>
